f03a553d95fd39710f06a55ef203ca30d92bc136
Finn Welsford-Ackroyd <welsfofinn@ecs.vuw.ac.nz>
2018-07-03 19:51:38 +1000

    Added Group class

---
+++ src/main/java/grid/Group.java
@@ -0,0 +1,19 @@
+package grid;
+
+/**
+ * Represents the three sudoku groups: row, column, and box.
+ * Contains methods for determining properties of a given group.
+ */
+public class Group {
+
+    public enum Type{ROW, COL, BOX};
+
+    public final Type type;
+    private Cell[] cells;
+
+    public Group(Type type, Cell[] cells) {
+        this.type = type;
+        this.cells = cells;
+    }
+
+}
